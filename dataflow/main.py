12a0f50e023f17e2c6ef238aa35ae55649af5df6
Nicha <NichaRoj@gmail.com>
2020-02-07 21:45:06 +0700

    Import csv and export to BigQuery

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,2 +1,10 @@
+import logging
+from storage import import_csv
+
 if __name__ == '__main__':
-  print('Hello world!')
+  logging.getLogger().setLevel(logging.INFO)
+  print('Starting pipeline...')
+  import_csv(
+    'gs://cubems-raw-data/8.csv',
+    'cubems-data-pipeline:test.temperature'
+  )
